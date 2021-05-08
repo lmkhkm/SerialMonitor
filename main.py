d58fcd4c7a5446f4f95d88bea6af24498c730949
lmkhkm <lmkhkm@naver.com>
2021-05-09 06:10:13 +0900

    If computer recieve char then print

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import serial
 
-ser = serial.Serial('COM7',115200, timeout=1)
+portName = 'COM7'
+baudRate = 115200
+
+ser = serial.Serial(portName, baudRate, timeout=1)
+
+
 while True:
     print("R: ", ser.readline())
